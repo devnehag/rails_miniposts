b0cf7935cb0f9834ab5d652f831b1a783ab91531
devnehag <gupta.neha2004@gmail.com>
2024-04-11 12:18:59 -0700

    "remember me" works after closing browser

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,27 +2,43 @@ module SessionsHelper
     # Logs in the given user.
   def log_in(user) # user argument is an instance of the User model.
     session[:user_id] = user.id #Body of the login method
-    end
-end
-
-  # Returns the current logged-in user (if any).
-
-  def current_user
-    if session[:user_id]
-    @current_user ||= User.find_by(id: session[:user_id])
-    end
   end
-    
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  # Returns the user corresponding to the remember token cookie.
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+      log_in user
+      @current_user = user
+    end
+   end
+  end  
   # Returns true if the user is logged in, false otherwise.
-
   def logged_in?
     !current_user.nil?
   end
 
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  # Logs out the current user.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
     end 
+end
 # session is a special kind of hash that Rails provides. 
 # It is used to store data that persists between requests. 
 # The data in the session is stored in the user's browser, so it can be used to maintain state during the user's interaction with the application.
