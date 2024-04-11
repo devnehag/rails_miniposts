c7b7d942ddb5eb3195b9a48d80b9339e548cdc40
devnehag <gupta.neha2004@gmail.com>
2024-04-11 14:10:26 -0700

    friendly farwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user #defined in session helper
      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+     redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
