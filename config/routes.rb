0b22fc8718f5b3e0f7c7c574ef98a1e3bd05c0ae
devnehag <gupta.neha2004@gmail.com>
2024-04-11 07:53:58 -0700

    remove webpack,node_modules, jquery working

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
