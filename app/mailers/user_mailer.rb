a9e7e4ccb226e6770c509070f24e7b65472f892f
devnehag <gupta.neha2004@gmail.com>
2024-04-13 16:11:56 -0700

    password reset implemented.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,9 +16,8 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "gupta.neha2004@gmail.com"
-  end
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  get 'users/new'
+  #get 'password_resets/new'
+  #get 'password_resets/edit'
+  #get 'users/new'
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
