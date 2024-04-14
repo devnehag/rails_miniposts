55cfaac41e37033adad53ad408c1ae44810283f9
devnehag <gupta.neha2004@gmail.com>
2024-04-14 16:28:08 -0700

    Add follow/unfollow functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
+# Users
 User.create!(name: "Neha Gupta",
 email: "gupta.neha2004@gmail.com",
-password: "password",
-password_confirmation: "password",
+password: "password@123",
+password_confirmation: "password@123",
 admin: true,
 activated: true,
 activated_at: Time.zone.now)
-# Generate a bunch of additional users.
-40.times do |n|
+99.times do |n|
 name = Faker::Name.name
-email = "peace-#{n+1}@miniposts.com"
+email = "rails-#{n+1}@miniposts.com"
 password = "password"
 User.create!(name: name,
 email: email,
@@ -17,9 +17,16 @@ password_confirmation: password,
 activated: true,
 activated_at: Time.zone.now)
 end
-# Generate microposts for a subset of users.
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
 content = Faker::Lorem.sentence(word_count: 5)
 users.each { |user| user.microposts.create!(content: content) }
 end
+# Create following relationships.
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
