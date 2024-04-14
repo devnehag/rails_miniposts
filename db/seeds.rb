7f14a63bf2df09ef31c96ad86054fcada92edd5f
devnehag <gupta.neha2004@gmail.com>
2024-04-13 19:18:47 -0700

    Showing miniposts with pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-# Create a main sample user.
-# Create a main sample user.
 User.create!(name: "Neha Gupta",
-email: "nehag@miniposts.com",
+email: "gupta.neha2004@gmail.com",
 password: "password",
 password_confirmation: "password",
 admin: true,
@@ -19,7 +8,7 @@ activated_at: Time.zone.now)
 # Generate a bunch of additional users.
 40.times do |n|
 name = Faker::Name.name
-email = "neha-#{n+1}@miniposts.com"
+email = "peace-#{n+1}@miniposts.com"
 password = "password"
 User.create!(name: name,
 email: email,
@@ -27,4 +16,10 @@ password: password,
 password_confirmation: password,
 activated: true,
 activated_at: Time.zone.now)
+end
+# Generate microposts for a subset of users.
+users = User.order(:created_at).take(6)
+50.times do
+content = Faker::Lorem.sentence(word_count: 5)
+users.each { |user| user.microposts.create!(content: content) }
 end
