b24ef7b25ab760b1dde61ba01c9f9766dc6a2acc
devnehag <gupta.neha2004@gmail.com>
2024-04-13 21:22:43 -0700

    Miniposts created displays to user with pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,13 +46,7 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted"
     redirect_to users_url
     end 
-  # Confirms a logged-in user.
-def logged_in_user
-  unless logged_in?
-    store_location
-    flash[:danger] = "Please log in."
-    redirect_to login_url
-  end
+  
   end
   # Confirms the correct user.
 def correct_user
