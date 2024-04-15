933127640ffeacb63b48767f83468a4bb59a9123
devnehag <gupta.neha2004@gmail.com>
2024-04-14 17:04:32 -0700

    Update follow and unfollow forms with ajax

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,9 +72,14 @@ class User < ApplicationRecord
         reset_sent_at < 2.hours.ago
     end
 
+    # Returns a user's status feed.
     def feed
-        Micropost.where("user_id = ?", id)
-        end
+        following_ids = "SELECT followed_id FROM relationships
+                        WHERE follower_id = :user_id"
+        Micropost.where("user_id IN (#{following_ids})
+                        OR user_id = :user_id", user_id: id)
+    end
+    
     # Follows a user.
     def follow(other_user)
         following << other_user
